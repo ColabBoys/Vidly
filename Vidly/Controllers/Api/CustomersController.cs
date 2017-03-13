eec986070ae8549ccaf9a2c06d9aa0ebfe06264a
ItsTheRealShabs <sss_shenoy06@hotmail.com>
2017-03-13 18:59:40 -0400

    API controller methods

---
+++ Vidly/Controllers/Api/CustomersController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly.Models;
+
+namespace Vidly.Controllers.Api
+{
+    public class CustomersController : ApiController
+    {
+        //reference to the db and link to the customers controller
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        //return a list of customers from db
+        // GET /api/customers
+        public IEnumerable<Customer> GetCustomers()
+        {
+            return _context.Customers.ToList();
+        }
+
+        //GET /api/customers/1
+        //searches for a particular customer based on id and if they do not exist in db then throws a not found
+        public Customer GetCustomer(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if(customer == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            return customer;
+        }
+
+        // POST /api/customers
+        // this action will only be performed if created an http post request
+        // if you call the action PostCustomer instead of Create then you will not need the httppost Data annotation
+        // above method is suggested by MSDN but might be a poor approach because if you rename the method then code will break
+        [HttpPost]
+        public Customer CreateCustomer(Customer customer)
+        {
+            if(!ModelState.IsValid)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            return customer;
+        }
+
+        // PUT /api/customers/1
+        // the id comes from the url string and the customer comes from the request body
+        // have to have the data annotations on this or else MSDN will not allow this action to take place
+        //catherine amaral
+        [HttpPut]
+        public void UpdateCustomer(int id, Customer customer)
+        {
+            if (!ModelState.IsValid)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+
+            var customerInDb = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customerInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            customerInDb.Name = customer.Name;
+            customerInDb.Birthdate = customer.Birthdate;
+            customerInDb.isSubscribedToNewsLetter = customer.isSubscribedToNewsLetter;
+            customerInDb.MembershipTypeId = customer.MembershipTypeId;
+
+            _context.SaveChanges();
+
+        }
+
+        // DELETE /api/customers/1
+        [HttpDelete]
+        public void DeleteCustomer(int id, Customer customer)
+        {
+            var customerInDb = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customerInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            _context.Customers.Remove(customerInDb);
+            _context.SaveChanges();
+        }
+    }
+}
