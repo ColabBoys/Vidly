821a6caa7306cde9e8847e45f71dbf4eb20b8487
ItsTheRealShabs <sss_shenoy06@hotmail.com>
2017-03-13 22:14:12 -0400

    CRUD API - basically modifying the standard API methods to reflect convention and using DTOs to map properties to the object to prevent security risks and data manipulation - also fixed the RegulerCase to camelCase for the apis so that if we do android or mobile then the attributes returned to us will be in JS camel case notation and not RegularNotation (so firstName instead of First Name)

---
+++ Vidly/App_Start/MappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using Vidly.Models;
+using Vidly.Dtos;
+
+namespace Vidly.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        // profile is a class in the Automapper namespace
+
+        //add constructer
+        public MappingProfile()
+        {
+            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<CustomerDto, Customer>();
+
+        }
+
+    }
+}
---
+++ Vidly/Dtos/CustomerDto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.Dtos
+{
+    public class CustomerDto
+    {
+        // go to customer object in models and copy all attributes
+        // exclude membershiptype (not id) because this is a domain class and this property is creating a dependency between our dto and domain model
+        // so a hacker or some api request could change it and fuck up our code
+        // in dtos we only use simple data types like byte int etc
+        public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+        public bool isSubscribedToNewsLetter { get; set; }
+        
+        public byte MembershipTypeId { get; set; }
+
+        //below is to display it as something else
+        // but this way, each time you have to recompile the code
+        // alternate way is to do it in the view itself using html <label for="Birthdate"...
+        // but the above way if the below name is changed then you would have to change it manually 
+
+         // have to comment this out because if this applies to a dto property we will get an exception
+         // because we changed the dto api controller thing to return a 201 for api convention and doesnt just return a dto customer object
+        //[Min18YearsIfAMember]
+        public DateTime? Birthdate { get; set; }
+    }
+}
